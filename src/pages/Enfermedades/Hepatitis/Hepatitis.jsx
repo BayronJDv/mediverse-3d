ba593086535d435d338f3f97eb717394e7a65e7d
David Enciso <david.enciso@correounivalle.edu.co>
2025-04-29 16:49:24 -0500

    cambios adicionales

--- src/pages/Enfermedades/Hepatitis/Hepatitis.jsx
+++ src/pages/Enfermedades/Hepatitis/Hepatitis.jsx
@@ -40,15 +40,15 @@ const Hepatitis = () => {
                         <OrbitControls />
                         <ambientLight intensity={1.5} />
                         <directionalLight position={[5, 5, 5]} intensity={2}
-                        castShadow 
-                        shadow-mapSize-width={1024}
-                        shadow-mapSize-height={1024}
-                        shadow-camera-near={0.5}
-                        shadow-camera-far={20}
-                        shadow-camera-left={-10}
-                        shadow-camera-right={10}
-                        shadow-camera-top={10}
-                        shadow-camera-bottom={-10}
+                            castShadow 
+                            shadow-mapSize-width={1024}
+                            shadow-mapSize-height={1024}
+                            shadow-camera-near={0.5}
+                            shadow-camera-far={20}
+                            shadow-camera-left={-10}
+                            shadow-camera-right={10}
+                            shadow-camera-top={10}
+                            shadow-camera-bottom={-10}
                         />
                         <HepatitisCell scale={1.5} />
                     </Canvas>
