586fcbd0eab2fb97f5471a8db8ea0c8f3035860b
David Enciso <david.enciso@correounivalle.edu.co>
2025-06-24 01:17:17 -0500

    Resuelto conflicto entre daeg y main

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter,Route,Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
+
 import Layout from './layout/Layout.jsx'
 import Home from './pages/home/Home.jsx'
 import Cirrocis from './pages/Enfermedades/cirrocis/Cirrocis.jsx'
 import HigadoGraso from './pages/Enfermedades/HigadoGraso/HigadoGraso.jsx'
 import Hepatitis from './pages/Enfermedades/Hepatitis/Hepatitis.jsx'
-import CancerHigado from './pages/Enfermedades/CancerHigado/CancerHigado.jsx' 
+import CancerHigado from './pages/Enfermedades/CancerHigado/CancerHigado.jsx'
 import Patologias from './pages/patologias/patologias.jsx'
-
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import Quiz from './pages/Quiz/Quiz.jsx'
 
 createRoot(document.getElementById('root')).render(
- 
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Learn" element={<Home />} />
-          <Route path="/Learn/Cirrocis" element={<Cirrocis />} />
-          <Route path="/Learn/HigadoGraso" element={<HigadoGraso />} />
-          <Route path="/Learn/Hepatitis" element={<Hepatitis />} />
-          <Route path="/Learn/CancerHigado" element={<CancerHigado />} /> 
-          <Route path="/Patologias" element={<Patologias />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
- 
+  <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Learn" element={<Home />} />
+        <Route path="/Learn/Cirrocis" element={<Cirrocis />} />
+        <Route path="/Learn/HigadoGraso" element={<HigadoGraso />} />
+        <Route path="/Learn/Hepatitis" element={<Hepatitis />} />
+        <Route path="/Learn/CancerHigado" element={<CancerHigado />} />
+        <Route path="/Patologias" element={<Patologias />} />
+        <Route
+          path="/quiz"
+          element={
+            <ProtectedRoute>
+              <Quiz />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Layout>
+  </BrowserRouter>
 )
-
-
