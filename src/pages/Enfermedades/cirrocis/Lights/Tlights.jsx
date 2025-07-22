f019b570233cbe0fb9a3bbe4e3f04401e8caea6d
BayronJDv <bayron.jojoa.dv@gmail.com>
2025-07-22 17:39:51 -0500

    final details cirrocis

--- src/pages/Enfermedades/cirrocis/Lights/Tlights.jsx
+++ src/pages/Enfermedades/cirrocis/Lights/Tlights.jsx
@@ -4,13 +4,25 @@ const Clights = () => {
 
 return (
     <>
-        <ambientLight intensity={1} />
+        <ambientLight intensity={0.5} />
+              <directionalLight
+        position={[1, 1, -1]}
+        intensity={2}
+        color={"#ffae42"}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-5}
+        shadow-camera-right={5}
+        shadow-camera-top={5}
+        shadow-camera-bottom={-5}
+      /> 
         <spotLight
             color={'red'}
             position={[0.2, 0.5, 0.6]}
             angle={Math.PI / 4}
             penumbra={1}
-            intensity={8}
+            intensity={5}
             castShadow
             />
            <spotLight
@@ -18,11 +30,11 @@ return (
             position={[3, 0.5, 0.6]}
             angle={Math.PI / 4}
             penumbra={1}
-            intensity={8}
+            intensity={3}
             castShadow
             />
 
-        {/*<Environment preset="city" />*/}
+        <Environment preset= "city"/>
     </>
 )
 }
